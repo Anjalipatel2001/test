0adcf829e3331be27641907d7c8a455861cdceb7
Anjali <anjalipatel12576@gmail.com>
2023-01-30 15:14:28 +0530

    modified code

--- first.cpp
+++ first.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include <string.h>
 using namespace std;
 
 int main()
 {
+    string name;
     cout <<"Hello world" << endl;
+    cout <<"Please enter your name:" << endl;
+    getline(cin , name);
+    cout << name << endl;
+return 0;
 }
