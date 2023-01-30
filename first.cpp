8303a2e473d0fedd977b74ead55582d5c655c471
Anjali <anjalipatel12576@gmail.com>
2023-01-30 19:33:21 +0530

    Revert " Agian modified file"
    This reverts commit fbde717a6b48e9250a9f736574f9c701385683eb.

--- first.cpp
+++ first.cpp
@@ -5,13 +5,9 @@ using namespace std;
 int main()
 {
     string name;
-    int age;
     cout <<"Hello world" << endl;
     cout <<"Please enter your name:" << endl;
     getline(cin , name);
-    cout <<"Please enter your age" << endl;
-    cin >> age;
-
-    cout <<"my name is" <<' ' << name << ' ' <<"and i am" <<' '<< age <<"years old" <<endl;
+    cout << name << endl;
 return 0;
 }
