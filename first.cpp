f4cfeaf733e4886c3acb8bfaed5fd21029f49119
Anjali <anjalipatel12576@gmail.com>
2023-02-01 23:17:58 +0530

    Add code

--- first.cpp
+++ first.cpp
@@ -6,19 +6,11 @@ int main()
 {
     string name;
     int age;
-    cout <<"Hello world" << endl;
     cout <<"Please enter your name:" << endl;
     getline(cin , name);
     cout <<"Please enter your age" << endl;
     cin >> age;
-<<<<<<< HEAD
-    cout <<"enter your city as well" << endl; 
-    cout <<"my salary is twenty thousand rupees" << endl;
     cout <<"my birth year is 2001" <<endl;
-=======
-    cout <<"enter your city as well" << endl;
-    cout <<"add country"<<endl;
->>>>>>> master
 return 0;
 }
 
